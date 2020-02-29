3a03cacad11c221640c3b023c5c69da80fcc0b84
jaworra <john.a.worrall@gmail.com>
2020-02-29 21:03:54 +1000

    add pymc3 functionality

--- coin_trial.py
+++ coin_trial.py
@@ -1,5 +1,5 @@
 '''
-simple coin trial
+simple coin trial - traddional
 '''
 import random
 
@@ -10,7 +10,6 @@ def coin_trial():
             heads +=1
     return heads
 
-
 def simulate(n):
     trials = []
     for i in range(n):
@@ -18,3 +17,30 @@ def simulate(n):
     return(sum(trials)/n)
 
 print(simulate(10))
+
+'''
+coin flipping problem - utilising bayesian techniques (PyMC3)
+utilising gpu C++ and returns TensorVariable
+'''
+
+import numpy as np
+import scipy.stats as stats
+import pymc3 as pm
+import arviz as az
+
+az.style.use('arviz-white')
+
+np.random.seed(123)
+trials = 4
+theta_real = 0.35 # unknown value in a real experiment
+data = stats.bernoulli.rvs(p=theta_real, size=trials)
+print(data)
+
+'''
+with pm.Model() as our_first_model:
+
+    θ = pm.Beta('θ', alpha=1., beta=1.)
+    y = pm.Bernoulli('y', p=θ, observed=data)   
+    trace = pm.sample(1000, random_seed=123)
+az.plot_trace(trace)
+'''
